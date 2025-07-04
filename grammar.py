6d0c85359f1cb118f5daa6d25b19aa00cca57a3e
learngermanghana <learngermanghana@gmail.com>
2025-07-04 08:01:18 +0000

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -3046,6 +3046,30 @@ if tab == "My Results and Resources":
             mime="application/pdf"
         )
 
+            # --- Resources Section ---
+    st.markdown("---")
+    st.subheader("📚 Useful Resources")
+
+    st.markdown(
+        """
+**1. [A1 Schreiben Practice Questions](https://drive.google.com/file/d/1X_PFF2AnBXSrGkqpfrArvAnEIhqdF6fv/view?usp=sharing)**  
+Practice writing tasks and sample questions for A1.
+
+**2. [A1 Exams Sprechen Guide](https://drive.google.com/file/d/1UWvbCCCcrW3_j9x7pOuWug6_Odvzcvaa/view?usp=sharing)**  
+Step-by-step guide to the A1 speaking exam.
+
+**3. [German Writing Rules](https://drive.google.com/file/d/1o7_ez3WSNgpgxU_nEtp6EO1PXDyi3K3b/view?usp=sharing)**  
+Tips and grammar rules for better writing.
+
+**4. [A2 Sprechen Guide](https://drive.google.com/file/d/1TZecDTjNwRYtZXpEeshbWnN8gCftryhI/view?usp=sharing)**  
+A2-level speaking exam guide.
+
+**5. [B1 Sprechen Guide](https://drive.google.com/file/d/1snk4mL_Q9-xTBXSRfgiZL_gYRI9tya8F/view?usp=sharing)**  
+How to prepare for your B1 oral exam.
+        """
+    )
+
+
 if tab == "Admin":
     # --- Admin Auth ---
     if not st.session_state.get("is_admin", False):
