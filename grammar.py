36561feacc681731b709127665a68bdbe65c08ef
learngermanghana <learngermanghana@gmail.com>
2025-07-11 09:48:51 +0000

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -1177,7 +1177,7 @@ def get_a2_schedule():
         # DAY 9
         {
             "day": 9,
-            "topic": "Urlaub 3.9",
+            "topic": "Urlaub 4.9",
             "chapter": "4.9",
             "goal": "Discuss vacation plans.",
             "assignment": True,
