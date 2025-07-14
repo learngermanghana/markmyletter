628caed8faa1ca93a461130ca239ee6abd6fe919
learngermanghana <learngermanghana@gmail.com>
2025-07-14 10:01:15 +0000

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -1284,7 +1284,7 @@ def get_a2_schedule():
             "goal": "Talk about your favorite sport.",
             "instruction": "Watch the video, review grammar, and complete your workbook.",
             "video": "",
-            "grammarbook_link": "https://drive.google.com/file/d/1UohFnTmCwjCJHQU1etaGOBzZJTk2kauz/view?usp=sharing",
+            "grammarbook_link": "https://drive.google.com/file/d/1dGZjcHhdN1xAdK2APL54RykGH7_msUyr/view?usp=sharing",
             "workbook_link": "https://drive.google.com/file/d/1iiExhUj66r5p0SJZfV7PsmCWOyaF360s/view?usp=sharing"
         },
         # DAY 16
