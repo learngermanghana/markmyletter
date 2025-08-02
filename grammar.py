a801d4fa4035def65b9984461cc5af7a292376ac
learngermanghana <learngermanghana@gmail.com>
2025-08-02 14:30:55 +0000

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -103,10 +103,6 @@ def fetch_youtube_playlist_videos(playlist_id, api_key):
             break
     return videos
 
-import sqlite3
-import atexit
-import streamlit as st
-from datetime import date
 
 # ==== DB CONNECTION & INITIALIZATION ====
 
@@ -250,12 +246,10 @@ def has_sprechen_quota(student_code, limit=FALOWEN_DAILY_LIMIT):
 
     
 
-import streamlit as st
-import requests
-
 # ==== YOUTUBE CONFIG & HELPER ====
 
-YOUTUBE_API_KEY = st.secrets["youtube_api_key"]  # Or wherever you store your key
+YOUTUBE_API_KEY = "AIzaSyBA3nJi6dh6-rmOLkA4Bb0d7h0tLAp7xE4"
+
 
 YOUTUBE_PLAYLIST_IDS = {
     "A1": "PL5cbb7kVbA4wFqFkmblK5z7Cwr2AvAxkH",
@@ -6164,6 +6158,7 @@ if tab == "Schreiben Trainer":
                     [],
                 )
                 st.rerun()
+
 
 
 
