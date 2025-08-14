2b23c8977a8bdc7e5cf381f7536b6f089103f1a2
learngermanghana <learngermanghana@gmail.com>
2025-08-14 07:31:16 +0000

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -8,14 +8,20 @@ from typing import Optional
 # ==== Third-Party Packages ====
 import bcrypt
 import firebase_admin
+import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 import streamlit as st
 import streamlit.components.v1 as components
+from bs4 import BeautifulSoup
+from docx import Document
 from firebase_admin import credentials, firestore
+from fpdf import FPDF
+from gtts import gTTS
 from openai import OpenAI
 from streamlit.components.v1 import html as st_html
 from streamlit_cookies_manager import EncryptedCookieManager
+from streamlit_quill import st_quill
 
 # ---- Streamlit page config MUST be first Streamlit call ----
 st.set_page_config(
